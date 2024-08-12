42a981ba84b4475673f33885b72551db5f6dac6a
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-08-12 19:50:46 -0400

    feat: Added authorized_keys and basic mksymlink script

---
+++ mksymlinks.sh
@@ -0,0 +1,2 @@
+# TODO: REALLY need to make this more robust.  This is a quick and dirty script to make symlinks to my dotfiles.  It's not very good.
+ln -s authorized_keys $HOME/.ssh/authorized_keys 
